c610a8cd78e9ab212388b66c7a87d47b5effdcc7
Satvik Bhatnagar <satvikbhatnagar@gmail.com>
2022-08-06 23:36:58 +0530

    Revert "Revert "code for horizontal visibility graph (for small dataset)""
    This reverts commit 2d2d927d12e12d2781d461d87dffadb3211ed661.

---
+++ codes/library/horizontal_visibility_graph.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jun 26 10:10:05 2022
+
+@author: satvi
+"""
+
+from ts2vg import HorizontalVG
+import matplotlib.pyplot as plt
+import plot_graph_horizontal
+
+def hg(ds_ts):
+    ts = ds_ts['Close']
+
+    g = HorizontalVG(directed=None).build(ts)
+    ax=plt.gca()
+    plot_graph_horizontal.plot_graph(g, ax=ax, title="directed=None", arrow_heads=True)
